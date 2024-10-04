0f0b77919598c1c4bcde82036d7c81cdea904df2
pPande199803 <96557963+pPande199803@users.noreply.github.com>
2024-10-04 11:04:39 +0530

    navbar created

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isUserLogin: boolean = false;
+  isUserNotLogin: boolean = false;
+
+  userValidToken: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.userValidToken = localStorage.getItem('loginValidToken')
+    if (this.userValidToken =='') {
+      this.isUserNotLogin = true;
+      this.isUserLogin = false;
+    } else {
+      this.isUserLogin = true;
+      this.isUserNotLogin = false;
+    }
+
+
+  }
 
 }
